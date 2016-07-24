734288812581283bc7e76b45a79d3e40b779b70e
leovannierop <leo.van.nierop@gmail.com>
2016-07-24 20:19:02 +0200

    done?

--- jupyter_server_first_boot.sh
+++ jupyter_server_first_boot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 FLAG="/var/log/firstboot_jupyter.log"
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+USER_NAME=
 if [ ! -f $FLAG ]; then
     echo "installing configured jupyter server. This may take a while."
     yum -y update
@@ -16,7 +17,9 @@ if [ ! -f $FLAG ]; then
     PATH=$PATH:/usr/local/bin
     export PATH
     mkdir $USER_HOME/.jupyter
+    chown $USER_NAME:$USER_NAME $USER_HOME/.jupyter
     touch $USER_HOME/.jupyter/jupyter_notebook_config.py
+    chown  $USER_HOME/.jupyter/jupyter_notebook_config.py
     printf 'NL\nNOORDHOLLAND\nAMSTERDAM\nME\nAWESOMESECTION\nLEO\nFAKE@SERVER.COM\n' | openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout $USER_HOME/.jupyter/sign.key -out $USER_HOME/.jupyter/sign.pem
     touch $FLAG
 else
