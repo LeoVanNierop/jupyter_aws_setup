cd35eef5149d2b9cda9d4c7d26432ac71833d132
leovannierop <leo.van.nierop@gmail.com>
2016-07-24 15:48:33 +0200

    basic script python only

---
+++ jupyter_server_first_boot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FLAG="/var/log/firstboot_jupyter.log"
+if [ ! -f $FLAG ]; then
+    echo "installing configured jupyter server. This may take a while."
+    yum -y update
+    yum -y install python34
+    yum -y install python34-pip
+    pip-3.4 install --upgrade pip
+    yum -y install python34-devel
+    yum -y install gcc
+    yum -y install gcc-c++
+    yum -y install freetype freetype-devel
+    yum -y install libpng libpng-devel
+    /usr/local/bin/pip3 install numpy scipy matplotlib pandas sympy nose ipython jupyter
+    jupyter notebook --generate-config
+    openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout ~/.jupyter/sign.key -out ~/.jupyter/sign.pem
+    touch $FLAG
+else
+    echo "No jupyter installation needed"
+fi
---
+++ setup_notebook_config.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/ipython
+from notebook.auth import passwd
+import os
+import sys
+home = os.getenv('HOME')
+notebook_dir = os.path.join(home, 'notebooks')
+os.makedirs(notebook_dir)
+pwd = passwd()
+with open(os.path.join(home, '.jupyter/jupyter_notebook_config.py'), 'a') as f:
+    f.write('c.NotebookApp.password = {}\n'.format(repr(pwd)))
+    f.write("c.NotebookApp.certfile = {}\n".format(repr(os.path.join(home, '.jupyter/sign.pem'))))
+    f.write("c.NotebookApp.keyfile = {}\n".format(repr(os.path.join(home, '.jupyter/sign.key'))))
+    f.write("c.NotebookApp.ip = '*'\n")
+    f.write("c.NotebookApp.open_browser = False\n")
+    f.write("c.NotebookApp.port = 9999\n")
+    f.write("c.NotebookApp.notebook_dir = {}\n".format(repr(notebook_dir)))
