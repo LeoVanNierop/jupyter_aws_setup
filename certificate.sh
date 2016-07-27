228ae13bb138f4d3d8282dca34b9216d4c53c7e5
leo <leo@qbiznl.com>
2016-07-27 13:42:34 +0200

    Ubuntu test version

---
+++ certificate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#get certification
+printf '\n' | ./certbot-auto certonly --standalone -d external_host
--- jupyter_server_first_boot_ubuntu.sh
+++ jupyter_server_first_boot_ubuntu.sh
@@ -24,10 +24,7 @@ if [ ! -f $FLAG ]; then
     chown $USER_NAME:$USER_NAME $USER_HOME/.jupyter/jupyter_notebook_config.py
     wget https://dl.eff.org/certbot-auto
     chmod a+x certbot-auto
-
-    #get certification
-    printf '\n' | ./certbot-auto certonly --standalone -d external_host
-
+    printf 'NL\nNOORDHOLLAND\nAMSTERDAM\nME\nAWESOMESECTION\nLEO\nFAKE@SERVER.COM\n' | openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout $USER_HOME/.jupyter/sign.key -out $USER_HOME/.jupyter/sign.pem
     touch $FLAG
 else
     echo "No jupyter installation needed"
