17bb1966efc33973658d95650b5c6cc561e51868
leo <leo@qbiznl.com>
2016-08-01 11:44:31 +0200

    forgot semicolon

--- certificate.sh
+++ certificate.sh
@@ -4,9 +4,8 @@
 
 #get certification
 
-if [$# eq 1]
-  then
-    printf '\n' | ./certbot-auto certonly --debug --standalone -d $1
+if [ $# eq 1]; then
+    printf '\n' | ./certbot-auto --debug --standalone -d $1
 else
   echo "supply domain name as command line argument"
 fi
