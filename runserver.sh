e25c99ed33d2ed712b8c0331a5313cb0c313dd9d
leo <leo@qbiznl.com>
2016-08-01 12:23:14 +0200

    trickery to get the certificate to work with manual certbot

--- runserver.sh
+++ runserver.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; cat index.html; } | nc -l 80; done
+while true; do { echo -e 'HTTP/1.1 200 OK\r\n'; cat /tmp/certbot/public_html/.well-known/acme-challenge/Ph_6XjD1VJuJlPMG56YUWhBGzrJHAo1iVgJ6UaPp7vs; } | nc -l 80; done
