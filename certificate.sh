64348fe4ef6c67edc92b36f8080082dc45c5a44f
leo <leo@qbiznl.com>
2016-08-01 11:48:05 +0200

    i hate bash....

--- certificate.sh
+++ certificate.sh
@@ -4,7 +4,7 @@
 
 #get certification
 
-if [ $# eq 1 ]; then
+if [ $# -eq 1 ]; then
     printf '\n' | ./certbot-auto --debug --standalone -d $1
 else
   echo "supply domain name as command line argument"
